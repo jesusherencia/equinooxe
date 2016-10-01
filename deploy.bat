200643cfa2422e526cff8f93f407d08f2bd24f9b
mohamedb <mohamedboullouz@gmail.com>
2016-10-02 00:43:28 +0200

    small utility to use from eclipse to deploy

---
+++ deploy.bat
@@ -0,0 +1,28 @@
+
+
+SET appServerLocation=C:\Users\mboullouz\Documents\equinooxe
+SET appServerLocationTarget=C:\Users\mboullouz\Documents\equinooxe\target
+SET tomcatAppFolder=C:\tomcat\webapps
+SET tomcatBinFolder=C:\tomcat\bin
+SET destFolderAndName=%tomcatAppFolder%\equinooxe.war
+ECHO server folder %appServerLocation%
+cd %appServerLocation%
+
+ECHO "lunch mvn package"
+call mvn package
+ECHO "enhancer..."
+call mvn datanucleus:enhance
+
+
+ 
+ECHO copie to %destFolderAndName%
+cd %appServerLocationTarget%
+xcopy /f /y *.war %destFolderAndName%  
+cd %tomcatBinFolder%
+call shutdown.bat
+call startup.bat
+
+ECHO ".................................."
+ECHO "............We are done..........."
+ECHO "............Enjoy because those moments of calm are rare!..........."
+ECHO ".................................."
