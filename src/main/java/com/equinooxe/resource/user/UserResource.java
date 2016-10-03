27f93c2028aa759121473e3eefd4e38a038324c3
mohamedb <mohamedboullouz@gmail.com>
2016-10-04 00:20:37 +0200

    feat:register users, secure path with shiro

--- src/main/java/com/equinooxe/resource/user/UserResource.java
+++ src/main/java/com/equinooxe/resource/user/UserResource.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -44,5 +45,16 @@ public class UserResource {
 	public Response getCurrentUser() {
 		return Response.status(Response.Status.OK).entity(userService.getAuthentificatedUser()).build();
 	}
+	
+	@Path("/register")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response register(BasicAuthDto dto) {
+        return Response.status(Response.Status.OK)
+        		.entity(userService.register(dto.getUsername(), dto.getUsername(), dto.getPassword()))
+        		.build();
+    }
+  
 
 }
