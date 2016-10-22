49726fa5899a7cd35d694fcbf4be854c9e97328c
mohamedb <mohamedboullouz@gmail.com>
2016-10-22 22:02:47 +0200

    Register Jackson hibernate module

--- src/main/java/com/equinooxe/resource/ApplicationConfig.java
+++ src/main/java/com/equinooxe/resource/ApplicationConfig.java
@@ -5,8 +5,12 @@
  */
 package com.equinooxe.resource;
 
+import com.equinooxe.resource.user.UserResource;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -31,15 +35,26 @@ public class ApplicationConfig extends Application {
     /**
      * This is how the Jersey - Jackson serialize the object returned as
      * Response Object
+     *
      * @return a Set
      */
     @Override
     public Set<Object> getSingletons() {
         Set<Object> set = new HashSet<>();
-        log.log(Level.INFO, "Enabling custom Jackson JSON provider");
-        set.add(new JacksonJsonProvider().configure(SerializationFeature.INDENT_OUTPUT, true)
-                .configure(DeserializationFeature.WRAP_EXCEPTIONS, true)
-        );
+        /**
+         * Register Hibernate module with Jackson mapper
+         * @see http://wiki.fasterxml.com/JacksonHowToCustomSerializers
+         * @see https://github.com/FasterXML/jackson-datatype-hibernate
+         */
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Hibernate5Module());
+        log.log(Level.INFO, "Enabling custom Jackson JSON provider and register Hibernate5Module ");
+        JacksonJsonProvider jacksonJsonProvider = new JacksonJsonProvider()
+                .configure(SerializationFeature.INDENT_OUTPUT, true)
+                .configure(DeserializationFeature.WRAP_EXCEPTIONS, true);
+        jacksonJsonProvider.setMapper(mapper);
+        set.add(jacksonJsonProvider);
+        
         set.add(new AuthorizationFilterFeature());
         set.add(new SubjectFactory());
         set.add(new AuthInjectionBinder());
@@ -60,7 +75,6 @@ public class ApplicationConfig extends Application {
         Set<Class<?>> resources = new java.util.HashSet<>();
         resources.add(AppCustomBasicAuthenticationFilter.class);/* @see the class doc */
         resources.add(com.equinooxe.resource.DatabaseOperationExeptionMapper.class);
-
         addRestResourceClasses(resources);
         return resources;
     }
@@ -68,6 +82,7 @@ public class ApplicationConfig extends Application {
     /**
      * Populated with all resources defined in the project. If required, comment
      * out calling this method in getClasses().
+     *
      * @warning Netbean IDE rewrites this methods autmatically
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
