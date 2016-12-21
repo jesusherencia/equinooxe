0f5134789036b74d9406404e4922e58013689d70
mohamedb <mohamedboullouz@gmail.com>
2016-12-21 21:18:39 +0100

    allow reconnect

---
+++ src/main/java/com/equinooxe/module/user/AuthController.java
@@ -0,0 +1,37 @@
+package com.equinooxe.module.user;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.equinooxe.domain.QUser;
+import com.equinooxe.domain.User;
+import com.equinooxe.security.AuthoritiesConstants;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@Controller
+@Secured(AuthoritiesConstants.USER)
+public class AuthController {
+	@Inject
+	EntityManager entityManager;
+	
+	@GetMapping("/reconnect")
+	public String reconnect(HttpServletRequest request){
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authentication = securityContext.getAuthentication();
+		UserDetails principal = (UserDetails) authentication.getPrincipal();
+		QUser qUser = QUser.user;
+		JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+		User u = queryFactory.selectFrom(qUser).where(qUser.login.eq(principal.getUsername())).fetchOne();
+		request.getSession().setAttribute("user", u);
+		return "redirect:/home";
+	}
+}
--- src/main/java/com/equinooxe/module/user/UserManagementController.java
+++ src/main/java/com/equinooxe/module/user/UserManagementController.java
@@ -1,9 +1,5 @@
 package com.equinooxe.module.user;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.validation.Valid;
@@ -11,30 +7,22 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.equinooxe.domain.QManagerUtilisateur;
-import com.equinooxe.domain.QUser;
 import com.equinooxe.domain.User;
 import com.equinooxe.repository.UserQueryRepository;
 import com.equinooxe.repository.UserRepository;
 import com.equinooxe.security.AuthoritiesConstants;
 import com.equinooxe.service.UserService;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
-import org.springframework.ui.Model;
 
 @Controller
 @Secured(AuthoritiesConstants.USER)
 public class UserManagementController {
-
-	@Inject
-	private UserRepository userRepository;
 
 	@Inject
 	private UserService userService;
@@ -49,15 +37,20 @@ public class UserManagementController {
 	UserQueryRepository userQueryRepo;
 
 	@GetMapping("/user/new")
-	public String showForm(@RequestParam(value = "id", required = false) Long id, UserForm userForm, Model uiModel) {
-		if (id != null && id > 0) {
-			User u = userQueryRepo.getOneById(id);
-			userForm = new UserForm(u);
-			uiModel.addAttribute("userForm",userForm);
-		}
+	public String showForm(UserForm userForm, Model uiModel) {
 		return "/user/form";
 	}
 
+	@GetMapping("/user/edit")
+	public String editForm(@RequestParam(value = "id", required = true) Long id, UserForm userForm, Model uiModel,
+			RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("flashMessage","editer .utilisateur.existant"); 
+		User u = userQueryRepo.getOneById(id);
+		userForm = new UserForm(u);
+		uiModel.addAttribute("userForm", userForm);
+		return "/user/form";
+	}
+ 
 	@PostMapping("/user/save")
 	public String save(@Valid UserForm userForm, BindingResult bindingResult, Model uiModel,
 			RedirectAttributes redirectAttributes) {
@@ -75,6 +68,7 @@ public class UserManagementController {
 		}
 
 		redirectAttributes.addAttribute("id", user.getId());
+		redirectAttributes.addFlashAttribute("flashMessage","operation.effectuee.avec.succes");
 		return "redirect:/user/show";
 	}
 
