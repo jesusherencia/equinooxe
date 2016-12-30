a79922fe12188809a6b43aa17b1b26ff24098269
mohamedb <mohamedboullouz@gmail.com>
2016-12-30 09:00:59 +0100

    chore

--- src/main/java/com/equinooxe/repository/UserQueryRepository.java
+++ src/main/java/com/equinooxe/repository/UserQueryRepository.java
@@ -5,38 +5,33 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.equinooxe.domain.QAuthority;
 import com.equinooxe.domain.QUser;
 import com.equinooxe.domain.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Component
 public class UserQueryRepository {
-	@Inject
-	EntityManager entityManager;
-	
+	 
 	QUser qUser;
 	JPAQueryFactory queryFactory;
- 
-	 
-	public JPAQueryFactory getQueryFactory(){
+	
+	@Autowired
+	public UserQueryRepository(EntityManager entityManager){
 		qUser = QUser.user;
 		queryFactory = new JPAQueryFactory(entityManager);
-		return queryFactory;
 	}
+	 
+     
 
 	public User getOneById(Long id) {
-		qUser = QUser.user;
-		queryFactory = new JPAQueryFactory(entityManager);
-		User u = getQueryFactory().selectFrom(qUser).where(qUser.id.eq(id)).fetchOne();
+		User u = queryFactory.selectFrom(qUser).where(qUser.id.eq(id)).fetchOne();
 		return u;
 	}
 	
 	public List<User> getAll(){
-		qUser = QUser.user;
-		queryFactory = new JPAQueryFactory(entityManager);
-		return   getQueryFactory().selectFrom(qUser).innerJoin(qUser.authorities).fetch();
+		return   queryFactory.selectFrom(qUser).innerJoin(qUser.authorities).fetch();
 	}
 }
