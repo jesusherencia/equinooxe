92feef056162c343329776d3c691c3e9c0709b5b
mohamedb <mohamedboullouz@gmail.com>
2017-01-16 20:25:32 +0100

    espace edit

--- src/main/java/com/equinooxe/module/espaces/EspaceController.java
+++ src/main/java/com/equinooxe/module/espaces/EspaceController.java
@@ -11,10 +11,12 @@ import javax.inject.Inject;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.equinooxe.domain.Espace;
 import com.equinooxe.repository.EspaceRepository;
+import com.equinooxe.repository.EtageRepository;
 import com.equinooxe.security.AuthoritiesConstants;
 
 @Controller
@@ -23,10 +25,18 @@ public class EspaceController {
 	@Inject
 	EspaceRepository espaceRepository;
 	
+	@Inject
+	EtageRepository etageRepository;
+	
 	@GetMapping("/espaces/espace/list")
 	public ModelAndView list(){
 		List<Espace>  espaces = new ArrayList<>();
 		return new ModelAndView("espaces/espace/list").addObject("espaces", espaces);
-		
+	}
+	
+	@GetMapping("/espaces/etage/{etageId}/espace/new")
+	public ModelAndView showForm(@PathVariable Long etageId, EtageFormModel etageFormModel) {
+		return new ModelAndView("espaces/espace/form").addObject("espaceFormModel",
+				new EtageFormModel(etageRepository.findOne(etageId)));
 	}
 }
