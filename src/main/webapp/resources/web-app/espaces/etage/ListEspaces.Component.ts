f8a2f29c62282eaa8a2ed34e18f2a54bc9130970
mohamedb <mohamedboullouz@gmail.com>
2017-01-19 07:24:29 +0100

    UI-list of spaces

--- src/main/webapp/resources/web-app/espaces/etage/ListEspaces.Component.ts
+++ src/main/webapp/resources/web-app/espaces/etage/ListEspaces.Component.ts
@@ -22,7 +22,38 @@ class ComponentOptionsImp implements IComponentOptions {
         deleteEspace: () => { },
     }
     public el = "#ListEspacesComponent";
-    public template = `<h1> ListEspacesComponent </h1> `;
+    public template = `
+    <table class="table smaller">
+                            <caption class="smaller">Espaces du:{{etage.nom }}</caption>
+                            <tr>
+                              <th># </th>
+                              <th>Nom </th>
+                              <th>Ajout </th>
+                            </th>
+                            <tr v-for="(espace,index) in etage.espaces" class="as-tr" title="epspace.description">
+                                    <td>  {{ espace.id}}       </td>
+                                    <td>  {{ espace.nom }}   </td>
+                                    <td>  <fr-datetime :frdate="espace.createdDate" ></fr-datetime> </td>
+                                    <td>
+                                    <div class="dropdown pull-right">
+                                        <a href="#" class="dropdown-toggle card-drop" title="Options" data-toggle="dropdown" aria-expanded="false">
+                                            <i class="zmdi zmdi-more-vert"></i>
+                                        </a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li>
+                                                <form  method="post" name="removeFormModel">
+                                                    <input type="hidden" name="id" value="espace.id"/>
+                                                    <input type="hidden" name="redirectTo" value=""/>
+                                                    <button v-on:click="deleteEspace($event,espace.id)" class="as-link">Supprimer</button>
+                                                </form>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                  </td>
+                            </tr>
+     </table> 
+    
+    `;
 }
 class DataModelHolder {
     public etage: EtageEntity = JSON.parse($("#EtageEntity").text());
