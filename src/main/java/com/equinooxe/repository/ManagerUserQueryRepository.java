8b45fc40b5fd7f9b113150f48383fc558be5f33e
mohamedb <mohamedboullouz@gmail.com>
2017-04-13 08:39:55 +0200

    Fix adding C Req

--- src/main/java/com/equinooxe/repository/ManagerUserQueryRepository.java
+++ src/main/java/com/equinooxe/repository/ManagerUserQueryRepository.java
@@ -20,8 +20,7 @@ import com.equinooxe.security.SecurityUtils;
 @Component
 public class ManagerUserQueryRepository extends AbstractQueryRespository<QManagerUser,ManagerUser> {
 	
-	@Inject
-	private UserRepository userRepository;
+ 
 	
 	@Autowired
 	public ManagerUserQueryRepository(EntityManager entityManager) {
@@ -40,15 +39,6 @@ public class ManagerUserQueryRepository extends AbstractQueryRespository<QManage
 	
 	public ManagerUser getCurrent() {
 		String userName= SecurityUtils.getCurrentUserLogin();
-		userRepository.findAll().forEach(u-> {
-			if(u.getLogin().contains(userName)) {
-				System.out.println("\n =================== \n Waw found: "+ u.getId()+ " "+ u.getEmail());
-			}
-			else {
-				System.out.println("\n ================= \n nop :: "+ u.getLogin()+" :: "+ u.getId()+ " :: "+ u.getEmail());
-			}
-			
-		});
 	    return queryFactory.selectFrom(qEntity).where(qEntity.login.eq(userName)).fetchOne();
 	}
 	
