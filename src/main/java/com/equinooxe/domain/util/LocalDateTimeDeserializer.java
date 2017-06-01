80f55614a974fd877280f9facd71a2c8efff1927
MohamedB <mohamedb@users.noreply.github.com>
2017-06-01 23:58:41 +0200

    Update LocalDateTimeDeserializer.java

--- src/main/java/com/equinooxe/domain/util/LocalDateTimeDeserializer.java
+++ src/main/java/com/equinooxe/domain/util/LocalDateTimeDeserializer.java
@@ -10,6 +10,8 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * Get a LocalDateTime from json "yyyy-MM-dd HH:mm:ss" format
+ * @todo 1: move some code to a static method
+         2: export the date format to config class/file
  * @author mboullouz
  */
 public class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
